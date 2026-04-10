85fd1caf5cd8b8de3d73cb6e1f152cf5322073a8
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-10 18:38:24 +0600

    book section done

---
+++ src/components/homepage/AllBooks.jsx
@@ -0,0 +1,26 @@
+import React, { use } from 'react';
+
+import BooksCard from '../../ui/BooksCard';
+const booksPromise = fetch('/booksData.json').then(res=>res.json())
+
+const AllBooks = () => {
+    const books = use(booksPromise);
+    console.log(books)
+    return (
+           <div className='my-12 container mx-auto'>
+            <h2 className='font-bold text-3xl text-center mb-6'>
+                Books
+            </h2>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {
+                books.map((book)=>{
+                    return <BooksCard book={book}/>
+                })
+            }
+          </div>
+            
+        </div>
+    );
+};
+
+export default AllBooks;
---
+++ src/ui/BooksCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FaRegStar } from 'react-icons/fa';
+
+const BooksCard = ({book}) => {
+    return (
+        
+        <div className="card bg-base-100  shadow-sm">
+  <figure className='p-6 '>
+    <img
+      src={book.image}
+      alt={book.bookName} className='rounded-xl h-[250px]' />
+  </figure>
+  <div className="card-body">
+    <div className='flex items-center gap-2'>
+        {
+            book.tags.map((tag)=>(
+
+            <div className="badge text-green-500 bg-green-100 font-bold">{tag}</div>
+        ))
+        }
+   
+
+    </div>
+    <h2 className="card-title">
+      <h2 className='font-bold text-xl'>{book.bookName}</h2>
+    </h2>
+    <p className='font-semibold text-lg'>{book.author}</p>
+    <div className="card-actions justify-between border-t border-dashed border-gray-300 pt-4">
+      <div className="font-semibold">{book.category}</div>
+      <div className="font-semibold flex gap-2 items-center">{book.rating}<FaRegStar/></div>
+    </div>
+  </div>
+</div>
+            
+        
+    );
+};
+
+export default BooksCard;
