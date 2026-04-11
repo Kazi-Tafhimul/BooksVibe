b99b16eeff14da09eb33cc24b322e2dfe0dfa446
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-11 22:01:11 +0600

    small issue fix

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -8,7 +8,12 @@ const BookProvider = ({children}) => {
     const [ storedBooks, setStoredBooks] = useState([]);
     const [wishList, setWishList] = useState([]);
     const handleMarkAsRead = (currentBook) =>{
+        const isExistInWishList = wishList.find((book) => book.bookId === currentBook.bookId);
         const isExistBook = storedBooks.find(book => book.bookId ===currentBook.bookId);
+        if(isExistInWishList){
+            toast.error("The book is already in wish list")
+            return;
+        }
         if(isExistBook){
             toast.error("The book is already exist");
         }
@@ -24,7 +29,7 @@ const BookProvider = ({children}) => {
         const isExistInReadList = storedBooks.find((book) => book.bookId === currentBook.bookId)
         const isExistBook = wishList.find(book => book.bookId ===currentBook.bookId);
         if(isExistInReadList){
-            toast.error("This book is already in wishlist")
+            toast.error("This book is already in readlist")
             return;
         }
 
